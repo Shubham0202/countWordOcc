ed5d430b77992e3d9c6bcc6c3626b1c312eec79b
Shubh <91457900+Shubham0202@users.noreply.github.com>
2025-03-25 13:32:09 +0530

    Initial commit

---
+++ extraq2.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+
+function countWordOccurrences(content, word) {
+  const regex = new RegExp(`\\b${word}\\b`, 'gi'); 
+  const matches = content.match(regex);
+  return matches ? matches.length : 0;
+}
+
+
+function countWordInFile(filePath, word) {
+
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Error reading file: ${err.message}`);
+      return;
+    }
+
+
+    const count = countWordOccurrences(data, word);
+    console.log(`The word "${word}" occurs ${count} time(s) in the file.`);
+  });
+}
+
+
+const filePath = path.join(__dirname, 'file2.txt'); 
+const wordToCount = 'hello'; 
+countWordInFile(filePath, wordToCount);
+
